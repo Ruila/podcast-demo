5e1157e762515caa39ea331a6934d522dbd26185
William0155 <william.lin@nextdrive.io>
2022-06-01 00:17:37 +0800

    feat: can drag progress bar to control time of music

--- src/components/player/MusicPlayer.vue
+++ src/components/player/MusicPlayer.vue
@@ -6,7 +6,6 @@
       src="https://files.soundon.fm/1611952660711-11c4cb94-968c-43fe-804d-25830fbd0338.jpeg"
       class="w-[80px] h-[80px] mr-4"
       alt="image"
-      @click="showCurrent"
     />
     <div class="mr-4" @click="togglePlay">
       <font-awesome-icon
@@ -18,26 +17,33 @@
     </div>
     <div>{{ minTime }}</div>
     <input
-      v-model="progressBarValue"
-      class="mx-4"
+      :value="progressBarValue"
+      @input="onInput"
+      @mousedown="MouseDown"
+      @mouseup="MouseUp"
+      class="mx-4 w-[600px]"
       type="range"
       min="0"
-      max="100"
+      :max="progressBarMaxValue"
     />
     <div>{{ maxTime }}</div>
-    <div>
-      <audio ref="audioRef" muted controls @canplay="canPlay">
-        <source
-          src="https://chtbl.com/track/4B4E1D/rss.soundon.fm/rssf/954689a5-3096-43a4-a80b-7810b219cef3/feedurl/17dfa81b-b307-4ac6-aeef-9ac624c41549/rssFileVip.mp3?timestamp=1653732572150"
-          type="audio/mpeg"
-        />
-      </audio>
-    </div>
+    <audio
+      ref="audioRef"
+      controls
+      class="hidden"
+      @canplay="canPlayHandler"
+      @timeupdate="timeUpdateHandler"
+    >
+      <source
+        src="https://chtbl.com/track/4B4E1D/rss.soundon.fm/rssf/954689a5-3096-43a4-a80b-7810b219cef3/feedurl/17dfa81b-b307-4ac6-aeef-9ac624c41549/rssFileVip.mp3?timestamp=1653732572150"
+        type="audio/mpeg"
+      />
+    </audio>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
 import type { Ref } from "@vue/reactivity";
 import { convertToTime } from "@/utils/convertToTime";
 const props = defineProps({
@@ -51,7 +57,10 @@ const props = defineProps({
 });
 const audioRef: Ref = ref<HTMLAudioElement>();
 const playing: Ref = ref<boolean>(false);
+const dragging: Ref = ref<boolean>(false);
 const progressBarValue: Ref = ref<number>(0);
+const progressBarMaxValue: Ref = ref<number>(0);
+const currentTime: Ref = ref<number>(0);
 const minTime: Ref = ref<string>("0:00");
 const maxTime: Ref = ref<string>("0:00");
 function togglePlay() {
@@ -62,22 +71,24 @@ function togglePlay() {
   }
   playing.value = !playing.value;
 }
-function showCurrent(): void {
-  console.info(
-    "showCurrent",
-    audioRef.value.currentTime,
-    typeof audioRef.value.duration
-  );
-}
-function canPlay(): void {
+function canPlayHandler(): void {
   maxTime.value = convertToTime(audioRef.value.duration);
+  progressBarMaxValue.value = Math.trunc(audioRef.value.duration);
 }
-onMounted(() => {
-  console.info(
-    "onMounted",
-    audioRef.value.currentTime,
-    audioRef.value.duration
-  );
-});
+function timeUpdateHandler(e: Event): void {
+  if (!dragging.value) {
+    progressBarValue.value = (e.target as HTMLAudioElement).currentTime;
+  }
+}
+function onInput(e: Event): void {
+  currentTime.value = (e.target as HTMLInputElement).value;
+}
+function MouseDown(): void {
+  dragging.value = true;
+}
+function MouseUp(): void {
+  audioRef.value.currentTime = currentTime.value;
+  dragging.value = false;
+}
 </script>
 <style scoped></style>
