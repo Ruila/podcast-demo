2d629db517fadfc3aca55b614068188694052685
Ruila <4momolmr@gmail.com>
2022-06-05 12:48:00 +0800

    fix: fix calculator

--- src/utils/convertToTime.ts
+++ src/utils/convertToTime.ts
@@ -1,7 +1,7 @@
 export const convertToTime = (value: number) => {
   const total = Math.trunc(value);
   const min = Math.trunc(total / 60);
-  const sec = Math.trunc(min > 0 ? total % min : total);
+  const sec = Math.trunc(min > 0 ? total % 60 : total);
   const showMin = min > 9 ? `${min}` : `0${min}`;
   const showSec = sec > 9 ? `${sec}` : `0${sec}`;
   return `${showMin}:${showSec}`;
